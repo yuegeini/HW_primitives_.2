d6c7a3332301b5717d39bc25def330ad654f3d58
Name <barmatine99@gmail.com>
2023-08-11 19:09:24 +0300

    Init

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter additional money: ");
+
+        int check = myObj.nextInt() + 100;
+
+        int add = (check - 100) / 100;
+        if(check > 1100) {
+            check += add;
+        } else {
+            add = 0;
+        }
+        System.out.println("New check: " + check);
+        System.out.println("Bonus money: " + add);
+
+    }
+}
